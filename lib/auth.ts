0355be435bde0781ea8d324e0e0b8f11513bb0b0
Abhishek Sharma <akstenx@gmail.com>
2024-07-27 14:29:27 +0530

    auth login testing

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,16 +5,16 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { accounts, sessions, users, verificationTokens } from "@/db/schema"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db, {
-    usersTable: users,
-    accountsTable: accounts,
-    sessionsTable: sessions,
-    verificationTokensTable: verificationTokens,
-  }),
+  // adapter: DrizzleAdapter(db, {
+  //   usersTable: users,
+  //   accountsTable: accounts,
+  //   sessionsTable: sessions,
+  //   verificationTokensTable: verificationTokens,
+  // }),
   providers: [
     Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      clientId: process.env.AUTH_GOOGLE_IDa,
+      clientSecret: process.env.AUTH_GOOGLE_SECRETa,
     }),
   ],
   debug: true,
