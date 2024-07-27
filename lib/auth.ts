9c5816d5107fa15b0dacfcbda18bdabd8f4d9bd9
Abhishek Sharma <akstenx@gmail.com>
2024-07-27 14:51:17 +0530

    auth file changes

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,12 +5,12 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { accounts, sessions, users, verificationTokens } from "@/db/schema"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // adapter: DrizzleAdapter(db, {
-  //   usersTable: users,
-  //   accountsTable: accounts,
-  //   sessionsTable: sessions,
-  //   verificationTokensTable: verificationTokens,
-  // }),
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
